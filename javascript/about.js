a8e77a36f5763d17b1423833bdaee4c6196a25f5
khan <khanetmoi@gmail.com>
2022-05-18 12:50:08 +0000

    mobile version of the capstone project

---
+++ javascript/about.js
@@ -0,0 +1,7 @@
+const shamburger = document.querySelector('.shamburger');
+const navlist2 = document.querySelector('#navList2');
+
+shamburger.addEventListener('click', () => {
+    console.log('clicked');
+    navlist2.classList.toggle('disappears');
+})
--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,32 @@
-console.log('start of the project')
+const more = document.querySelector('#more');
+const members = document.querySelector('.members');
+
+var state = 0;
+
+// more
+more.addEventListener('click', function() {
+    // members.classList.toggle('add');
+    if(state == 0){
+        members.style.height = 'auto'
+        more.innerText = 'less';
+        state +=1;
+    }
+    else {
+        members.style.height = '65vh'
+        more.innerText = 'more';
+        state -= 1;
+    }
+    
+})
+
+// hamburger button
+
+const hamburger = document.querySelector('.hamburger');
+const navlist = document.querySelector('#navList');
+
+
+
+hamburger.addEventListener('click', () => {
+    navlist.classList.toggle('disappear');
+})
+
