2a0cc8317fc6197cdb55679346c03444d65ca3be
khan <khanetmoi@gmail.com>
2022-05-19 19:45:09 +0000

    correcting errors pointed out by the reviewer

--- javascript/script.js
+++ javascript/script.js
@@ -50,49 +50,55 @@ const card = [
   {
     id: 1,
     image_id: 'im1',
-    title: 'Jone doe',
+    image_source: 'https://i.pravatar.cc/150?img=15',
+    title: 'shakya muni',
     subtitle: 'Director at the university of san fransisco',
     paragraph: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas deserunt necessitatibus reprehenderit quod natus,consequuntur assumenda, earum voluptatibus ducimus, molestias dolorum harum? Sit eveniet, quas facere quam temporibus assumenda itaque.',
   },
   {
     id: 2,
     image_id: 'im2',
-    title: 'Jane doe',
+    image_source: 'https://i.pravatar.cc/150?img=33',
+    title: 'karma kagyu',
     subtitle: 'Director at the university of san fransisco',
     paragraph: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas deserunt necessitatibus reprehenderit quod natus,consequuntur assumenda, earum voluptatibus ducimus, molestias dolorum harum? Sit eveniet, quas facere quam temporibus assumenda itaque.',
   },
   {
     id: 3,
     image_id: 'im3',
-    title: 'Jone doe',
+    image_source: 'https://i.pravatar.cc/150?img=8',
+    title: 'Drukpa kagyu',
     subtitle: 'Director at the university of san fransisco',
     paragraph: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas deserunt necessitatibus reprehenderit quod natus,consequuntur assumenda, earum voluptatibus ducimus, molestias dolorum harum? Sit eveniet, quas facere quam temporibus assumenda itaque.',
   },
   {
     id: 4,
     image_id: 'im4',
-    title: 'Jone doe',
+    image_source: 'https://i.pravatar.cc/150?img=11',
+    title: 'sangye menla',
     subtitle: 'Director at the university of san fransisco',
     paragraph: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas deserunt necessitatibus reprehenderit quod natus,consequuntur assumenda, earum voluptatibus ducimus, molestias dolorum harum? Sit eveniet, quas facere quam temporibus assumenda itaque.',
   },
   {
     id: 5,
     image_id: 'im5',
-    title: 'Jone doe',
+    image_source: 'https://i.pravatar.cc/150?img=45',
+    title: 'Samantabhadra',
     subtitle: 'Director at the university of san fransisco',
     paragraph: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas deserunt necessitatibus reprehenderit quod natus,consequuntur assumenda, earum voluptatibus ducimus, molestias dolorum harum? Sit eveniet, quas facere quam temporibus assumenda itaque.',
   },
   {
     id: 6,
     image_id: 'im6',
-    title: 'Jone doe',
+    image_source: 'https://i.pravatar.cc/150?img=55',
+    title: 'Lama Tsonkapa',
     subtitle: 'Director at the university of san fransisco',
     paragraph: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas deserunt necessitatibus reprehenderit quod natus,consequuntur assumenda, earum voluptatibus ducimus, molestias dolorum harum? Sit eveniet, quas facere quam temporibus assumenda itaque.',
   },
 ];
 
 const cards = card.map((fuji) => `<div class="member one">
-               <div id="${fuji.image_id}"></div>
+               <div id="${fuji.image_id}"> <img src="${fuji.image_source}" alt=""></div>
                <div class="reference">
                <h2>${fuji.title}</h2>
                <h3>${fuji.subtitle}</h3>
