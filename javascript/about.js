e8de0f584f1d2738ae9ff7fe7ebf3d3910f7333a
khan <khanetmoi@gmail.com>
2022-05-20 07:05:33 +0000

    correcting the errors pointed by the reviewer

--- javascript/about.js
+++ javascript/about.js
@@ -2,6 +2,5 @@ const shamburger = document.querySelector('.shamburger');
 const navlist2 = document.querySelector('#navList2');
 
 shamburger.addEventListener('click', () => {
-  console.log('clicked');
   navlist2.classList.toggle('disappears');
 });
